f50483ff3a2e53b5b7b7e3a19b46f82364d2b83f
Robert Lemmens <rj.lemmens@pm.me>
2021-08-08 21:01:39 +0200

    fix: bump version

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t rlemmens/oauth2-server-rs .
-docker push rlemmens/oauth2-server-rs
+docker build -t rlemmens/oauth2-server-rs:0.2.0 .
+docker push rlemmens/oauth2-server-rs:0.2.0
