73d90135261d3c3f11ab4a85b105b46a4c5697ac
Robert J. Lemmens <eksit@pm.me>
2022-02-07 03:05:21 +0000

    added dev

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+if [[ $1 == 'dev' ]]
+then
+echo "Building dev image"
+docker build -t rlemmens/oauth2-server-rs:dev .
+docker push rlemmens/oauth2-server-rs:dev
+else
 docker build -t rlemmens/oauth2-server-rs:0.2.0 .
 docker push rlemmens/oauth2-server-rs:0.2.0
+fi
