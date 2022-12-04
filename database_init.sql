81f50522c27faa49912788e7126434411bfd58a8
Robert Lemmens <rj.lemmens@pm.me>
2022-12-04 03:49:49 +0100

    update init script with uuid

--- database_init.sql
+++ database_init.sql
@@ -3,9 +3,10 @@ drop table if exists user_data;
 drop table if exists access_tokens;
 drop table if exists authorization_codes;
 drop table if exists clients;
+drop table if exists users;
 
 create table if not exists users (
-  id serial primary key,
+  id UUID primary key DEFAULT gen_random_uuid(),
   username varchar(50) not null unique,
   email varchar(100) not null unique,
   password varchar(512) not null
@@ -13,7 +14,7 @@ create table if not exists users (
 
 create table if not exists user_data (
   id serial primary key,
-  user_id integer,
+  user_id UUID,
   key varchar(512),
 
   foreign key (user_id) references users(id)
@@ -33,7 +34,7 @@ create table if not exists access_tokens (
   creation_time timestamp with time zone not null,
   scope varchar(255),
   token_type varchar(50) not null,
-  user_id integer,
+  user_id UUID,
   client_id UUID not null,
   device varchar(255) not null,
   issuer varchar(255) not null,
@@ -44,7 +45,7 @@ create table if not exists access_tokens (
 create table if not exists authorization_codes (
   id serial primary key,
   client_id UUID,
-  user_id integer,
+  user_id UUID,
   code varchar(255) not null,
   device varchar(255) not null,
   pcke_hash varchar(255),
@@ -65,4 +66,5 @@ create table if not exists login_sessions (
 
 alter table access_tokens add constraint unique_uid_cid unique (user_id, client_id, device);
 insert into clients (display_name, client_id, client_secret) values ('Mijn Client', 'top', 'top_321');
+insert into users (username, email, password) values ('test', 'test@test.nl', 'test');
 
